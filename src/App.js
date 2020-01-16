e95ef96617dab47d67324ef60b4dbde514e94cb3
Jordan-Shaw <jordan.shaw2400@gmail.com>
2020-01-16 12:50:01 +0000

    Changed some css, esp. the sidebar stuff

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Homepage from "./components/general/Homepage"
 import SingleArticle from "./components/articles/SingleArticle"
 import ArticlePage from "./components/articles/ArticlePage"
 import TopicsPage from "./components/topics/TopicsPage"
-import { stack as Menu } from 'react-burger-menu'
+import { bubble as Menu } from 'react-burger-menu'
 import { Link } from '@reach/router'
 import ErrorPage from './components/general/ErrorPage';
 
@@ -19,14 +19,14 @@ export default class App extends Component {
     const { username } = this.state
 
     return (
-      <div className="App">
-        <Menu>
+      <div className="outer-container">
+        <Menu pageWrapId={"page-wrap"}>
           <Link to="/" className="navItem">Home</Link>
           <Link to="/articles" className="navItem">Articles</Link>
           <Link to={`/users/${username}`} className="navItem">Profile</Link>
         </Menu>
+        <Header />
         <div id="page-wrap">
-          <Header />
           <Router className="pageBody">
             <Homepage path="/" />
             <ArticlePage path="/articles" username={username} />
